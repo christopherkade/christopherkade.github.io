f866c53cf4db0dfe9822a1fc06d4c1426fda4f9e
christopherkade <c.kade96@gmail.com>
2019-11-08 14:50:38 +0100

    💄 Update picture size

--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -18,8 +18,8 @@ const Author = ({ author, isIndex }: Props) => (
       <img
         src={withPrefix(author.photo)}
         className={styles['author__photo']}
-        width="75"
-        height="75"
+        width="125"
+        height="125"
         alt={author.name}
       />
     </Link>
