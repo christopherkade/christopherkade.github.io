440bb53b3af8ce942689317d760d8ffb3014b83c
Christopher Kade <c.kade96@gmail.com>
2021-07-23 16:13:52 +0200

    Fixed lint errors

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import moment from 'moment';
 import { Link } from 'gatsby';
 import type { Edges } from '../../types';
 import styles from './Feed.module.scss';
--- src/components/Sidebar/Author/Author.js
+++ src/components/Sidebar/Author/Author.js
@@ -32,7 +32,7 @@ const Author = ({ author, isIndex }: Props) => (
         <h2 className={styles['author__title']}>
           <Link className={styles['author__title-link']} to="/">{author.name}</Link>
         </h2>
-      )}
+    )}
     <p className={styles['author__subtitle']}>{author.bio}</p>
   </div>
 );
